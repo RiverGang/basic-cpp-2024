07bd8d6dd2f48321f07be3d1e57624f2a402821d
RiverGang <sory304@naver.com>
2024-03-15 13:57:49 +0900

    오전커밋

--- day01/ds01_Project1/ds01_Project1/for1.cpp
+++ day01/ds01_Project1/ds01_Project1/for1.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 
 using namespace std;
 
@@ -13,4 +13,45 @@ int main()
 		cout << i << endl;
 	}
 	return 0;
+}*/
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int* solution(const char* shirt_size[], int shirt_size_len) {
+    // Write code here.
+    int ret[sizeof(shirt_size_len)];
+    for (int i = 0; i < shirt_size_len; i++)
+    {
+        if (shirt_size[i] = "XS")
+            ret[0]++;
+        else if (shirt_size[i] = "S")
+            ret[1]++;
+        else if (shirt_size[i] = "M")
+            ret[2]++;
+        else if (shirt_size[i] = "L")
+            ret[3]++;
+        else if (shirt_size[i] = "XL")
+            ret[4]++;
+        else
+            ret[5]++;
+    }
+    int* answer = ret;
+    return answer;
+}
+
+// The following is main function to output testcase.
+int main() {
+    const char * shirt_size[] = { "XS", "S", "L", "L", "XL", "S" };
+    int shirt_size_len = 6;
+    int* ret = solution(shirt_size, shirt_size_len);
+
+    // Press Run button to receive output.       
+    printf("Solution: return value of the function is {");
+    for (int i = 0; i < 6; i++) {
+        if (i != 0) printf(", ");
+        printf("%d", ret[i]);
+    }
+    printf("} .\n");
 }
